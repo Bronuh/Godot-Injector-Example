8389372d4209371bc8fc7b18c9a5cc961b421070
Bronuh <bronyx@yandex.ru>
2024-09-01 16:22:59 +0300

    Make it more universal

--- ModLoader.gd
+++ ModLoader.gd
@@ -1,22 +1,25 @@
 extends Node
 
+var pck_path : String = "res://CatBait.pck"
 var main_path : String = "res://scenes/ui/main_menu.tscn"
 
 func _ready() -> void:
+	# Попытка поолучить путь к сцене из аргументов командной строки вроде --main=res://scenes/main/main.tscn
+	var arguments = {}
+	for argument in OS.get_cmdline_args():
+		if argument.find("=") > -1:
+			var key_value = argument.split("=")
+			arguments[key_value[0].lstrip("--")] = key_value[1]
+	
+	if arguments.has("pck"):
+		pck_path = arguments["pck"]
 	# Загрузка контента игры
-	var success = ProjectSettings.load_resource_pack("res://CatBait.pck")
+	var success = ProjectSettings.load_resource_pack(pck_path)
 	
 	if success:
-		# Попытка поолучить путь к сцене из аргументов командной строки вроде --main=res://scenes/main/main.tscn
-		var arguments = {}
-		for argument in OS.get_cmdline_args():
-			if argument.find("=") > -1:
-				var key_value = argument.split("=")
-				arguments[key_value[0].lstrip("--")] = key_value[1]
-		
 		# Если явно указан путь - используем его
-		if arguments["main"]:
-			main_path = arguments["main"]
+		if arguments.has("scene"):
+			main_path = arguments["scene"]
 		
 		print("Mod Loader started")
 		# Отложенный запуск искомой сцены, чтобы не конфликтовать с текущим вызовом _ready
